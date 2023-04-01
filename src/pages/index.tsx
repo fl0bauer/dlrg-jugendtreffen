5eb53e718311a109c459126e246892b8b66de120
Florian Bauer <fb.bauer@siemens.com>
2023-04-02 00:13:42 +0200

    chore(meta): add `title`, `description` and `favicon`
    Closes: #3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import Head from "next/head";
+
 export default function Home() {
-	return <></>;
+	return (
+		<>
+			<Head>
+				<title>DRLG Anmeldeformular Landesjugendtreffen 2023</title>
+				<meta name="description" content="Anmeldeformular für das DRLG Landesjugendtreffen 2023 in Blaubeuren" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.png" />
+			</Head>
+		</>
+	);
 }
