cee161420732f0a1fbc42735a85f17da07189d85
Florian Bauer <fb.bauer@siemens.com>
2023-04-04 00:20:19 +0200

    chore(components): support `htmlFor` on `label`

--- src/components/dropdown.component.tsx
+++ src/components/dropdown.component.tsx
@@ -3,17 +3,21 @@ import classNames from "classnames";
 
 const styles = {
 	container: "flex flex-col gap-1",
-	label: "block text-sm font-medium leading-6 text-gray-900 select-none",
+	label: "block w-fit text-sm font-medium leading-6 text-gray-900 select-none",
 	select: "block bg-white rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 outline-0 transition-all placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-500 disabled:bg-gray-100 disabled:cursor-not-allowed",
 };
 
-export default function Dropdown({ children, label, className, ...props }: DropdownProps) {
+export default function Dropdown({ children, id, label, className, ...props }: DropdownProps) {
 	const classes = classNames(styles.select, className);
 
 	return (
 		<div className={styles.container}>
-			{label && <label className={styles.label}>{label}</label>}
-			<select className={classes} {...props}>
+			{label && (
+				<label htmlFor={id} className={styles.label}>
+					{label}
+				</label>
+			)}
+			<select id={id} name={id} className={classes} {...props}>
 				{children}
 			</select>
 		</div>
--- src/components/input.component.tsx
+++ src/components/input.component.tsx
@@ -3,17 +3,21 @@ import classNames from "classnames";
 
 const styles = {
 	container: "flex flex-col gap-1",
-	label: "block text-sm font-medium leading-6 text-gray-900 select-none",
+	label: "block w-fit text-sm font-medium leading-6 text-gray-900 select-none",
 	input: "block bg-white rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 outline-0 transition-all placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-500 disabled:bg-gray-100 disabled:cursor-not-allowed",
 };
 
-export default function Input({ label, className, ...props }: InputProps) {
+export default function Input({ label, id, className, ...props }: InputProps) {
 	const classes = classNames(styles.input, className);
 
 	return (
 		<div className={styles.container}>
-			{label && <label className={styles.label}>{label}</label>}
-			<input className={classes} {...props} />
+			{label && (
+				<label htmlFor={id} className={styles.label}>
+					{label}
+				</label>
+			)}
+			<input id={id} name={id} className={classes} {...props} />
 		</div>
 	);
 }
