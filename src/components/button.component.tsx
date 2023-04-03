7b14ce7ca5829a7acd415e3de167a4c17aad6cd0
Florian Bauer <fb.bauer@siemens.com>
2023-04-03 21:45:09 +0200

    refactor(components): `button` with new typing and native `disabled`

--- src/components/button.component.tsx
+++ src/components/button.component.tsx
@@ -1,19 +1,17 @@
 import classNames from "classnames";
-import { ButtonProps, ButtonVariant } from "@/types/button.types";
-import { ComponentStyles, Styled } from "@/types/styled.type";
+import { ButtonProps } from "@/types/button.types";
 import { cs } from "@/lib/styles.lib";
 
-const styles: ComponentStyles<{ variant: Styled<ButtonVariant> }> = {
-	base: "inline-flex items-center justify-center gap-2 py-2 px-3 text-sm font-semibold select-none rounded-md transition-colors focus-visible:outline-2 focus-visible:outline-offset-2",
-	disabled: "text-white bg-gray-200 cursor-not-allowed",
+const styles = {
+	base: "inline-flex items-center justify-center gap-2 py-2 px-3 text-sm font-semibold select-none rounded-md transition-colors focus-visible:outline-2 focus-visible:outline-offset-2 disabled:text-white disabled:bg-gray-200 disabled:cursor-not-allowed",
 	variant: {
 		primary: "text-white bg-rose-600 shadow-md hover:bg-rose-500 focus-visible:outline-rose-500",
 		secondary: "text-rose-500 bg-rose-100 hover:bg-rose-200 focus-visible:outline-rose-500",
 	},
 };
 
-export default function Button({ children, variant = "primary", disabled, className, ...props }: ButtonProps) {
-	const classes = classNames(styles.base, cs(styles.variant[variant], !disabled), cs(styles.disabled, !!disabled), className);
+export default function Button({ children, variant = "primary", className, ...props }: ButtonProps) {
+	const classes = classNames(styles.base, cs(styles.variant[variant], !props.disabled), className);
 
 	return (
 		<button className={classes} {...props}>
