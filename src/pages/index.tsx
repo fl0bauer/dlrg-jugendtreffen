6e5a6cc7f39bf73fca12af90f921dfaf16821feb
Florian Bauer <fb.bauer@siemens.com>
2023-04-06 01:04:50 +0200

    chore(pages): use `frame` component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "@/components/head.component";
 import { InferGetServerSidePropsType } from "next";
 import LocationFormular from "@/formulars/location";
 import { fetchAssociations } from "@/prisma/location";
+import Frame from "@/components/frame.component";
+import useTranslation from "next-translate/useTranslation";
 
 export const getServerSideProps = async () => {
 	const associations = await fetchAssociations();
@@ -12,13 +14,17 @@ export const getServerSideProps = async () => {
 };
 
 export default function Home({ associations }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	const { t } = useTranslation("forms");
+
 	return (
 		<>
 			<Head />
 
-			<div className="p-20">
-				<LocationFormular associationsWithDependencies={associations} onValidFormSubmit={(values) => console.log(values)} />
-			</div>
+			<Frame heading={t("location.heading")} description={t("location.description")}>
+				<div className="flex flex-col mx-auto mt-16 max-w-xl sm:mt-20">
+					<LocationFormular associationsWithDependencies={associations} onValidFormSubmit={(values) => console.log(values)} />
+				</div>
+			</Frame>
 		</>
 	);
 }
