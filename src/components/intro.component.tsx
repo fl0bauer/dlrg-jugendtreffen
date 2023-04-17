40a18ace2c7254884be49f489a604ddab638247c
Florian Bauer <fb.bauer@siemens.com>
2023-04-17 10:06:26 +0200

    fix(components): keep intro animation state at end

--- src/components/intro.component.tsx
+++ src/components/intro.component.tsx
@@ -2,14 +2,14 @@ import NextImage from "next/image";
 import { useState } from "react";
 
 const styles = {
-	container: `z-10 fixed inset-0 bg-white flex items-center justify-center animate-[ping_2000ms_ease-in-out_1000ms]`,
+	container: `z-10 fixed inset-0 bg-white flex items-center justify-center pointer-events-none animate-[ping_2000ms_ease-in-out_1000ms_forwards]`,
 	image: "animate-bounce select-none pointer-events-none",
 };
 
 export default function Intro() {
 	const [animationStopped, setAnimationStopped] = useState(false);
 
-	setTimeout(() => setAnimationStopped(true), 2000 + 1000 - 50);
+	setTimeout(() => setAnimationStopped(true), 2000 + 1000);
 
 	if (animationStopped) {
 		return <></>;
