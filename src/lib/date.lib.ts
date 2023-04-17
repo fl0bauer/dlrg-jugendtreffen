c975e219a263a68cf260e6728b4c59e778ef2138
Florian Bauer <fb.bauer@siemens.com>
2023-04-18 01:00:37 +0200

    fix(lib): empty date function input

--- src/lib/date.lib.ts
+++ src/lib/date.lib.ts
@@ -17,6 +17,8 @@ export function getAge(birthDate: string, untilDate?: string): number {
  * @param date
  */
 export function formatDate(date: string) {
+	if (!date) return "";
+
 	const [year, month, day] = date.split("-");
 	return `${day}.${month}.${year}`;
 }
