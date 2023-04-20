449c5b3a4ddd615dfea616e62fea5495b145e311
Florian Bauer <fb.bauer@siemens.com>
2023-04-20 12:43:07 +0200

    feat(formulars): add `link` to `legal`

--- src/formulars/legal.formular.tsx
+++ src/formulars/legal.formular.tsx
@@ -2,6 +2,8 @@ import { Form } from "@/components/form.component";
 import Checkbox from "@/components/checkbox.component";
 import useTranslation from "next-translate/useTranslation";
 import { useFormContext } from "react-hook-form";
+import Trans from "next-translate/Trans";
+import Link from "@/components/link.component";
 
 export default function LegalFormular() {
 	const { t } = useTranslation("legal.formular");
@@ -15,8 +17,15 @@ export default function LegalFormular() {
 	return (
 		<Form>
 			<Form.Group columns={1}>
-				<Checkbox id="legal-accept" label={getLabel("accept")} error={getErrors("accept")} required {...register("accept")} />
+				<Checkbox id="legal-accept" label={<Trans i18nKey={getLabel("accept")} components={[<Link key="legal-link" href={process.env.NEXT_PUBLIC_LEGAL_LINK} target="_blank" />]} />} error={getErrors("accept")} required {...register("accept")} />
 			</Form.Group>
 		</Form>
 	);
 }
+
+// <span>
+// 							<a href={"#"} className="text-blue-400 hover:underline underline-offset-4">
+// 								Teilnahmebedingungen
+// 							</a>{" "}
+// 	gelesen und akzeptiert
+// 						</span>
--- src/types/checkbox.types.ts
+++ src/types/checkbox.types.ts
@@ -2,7 +2,7 @@ import { ComponentPropsWithoutRef, ReactElement } from "react";
 import { TooltipProps } from "@/types/tooltip.types";
 
 export interface CheckboxProps extends ComponentPropsWithoutRef<"input"> {
-	label?: string;
+	label?: string | ReactElement;
 	error?: string;
 	tooltip?: ReactElement<TooltipProps>;
 }
